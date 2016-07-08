1bf642120d982c81aeb21b8f3875f463bb4b857e
Atanasiu <Andreww0007@gmail.com>
2016-07-08 10:12:26 +0200

    Added Line Chart and SearchEnginesFragment

---
+++ app/src/main/java/dk/siteimprove/internship/atanasiu/andrei/analyticapplication/SearchEnginesFragment.java
@@ -0,0 +1,209 @@
+package dk.siteimprove.internship.atanasiu.andrei.analyticapplication;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Base64;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+
+public class SearchEnginesFragment extends Fragment implements View.OnClickListener
+{
+    LineChart chart;
+    ArrayList<LineDataSet> dataSets;
+    ArrayList<String> xAxis;
+    ProgressBar progressBar;
+    static final String API_KEY = "ebd8cdc10745831de07c286a9c6d967d";
+    static final String API_URL = "https://api.siteimprove.com/v2/sites/73617/analytics/traffic_sources/search_engines";
+
+    private OnFragmentInteractionListener mListener;
+
+    public SearchEnginesFragment() {  }    // Required empty public constructor
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.fragment_search_engines, container, false);
+        progressBar = (ProgressBar) rootView.findViewById(R.id.progressBar);
+
+        chart = (LineChart) rootView.findViewById(R.id.chart);
+        new RetrieveFeedTask().execute();
+
+        Button queryButton = (Button) rootView.findViewById(R.id.queryButton);
+        queryButton.setOnClickListener(this);
+        // Inflate the layout for this fragment
+        return  rootView;
+
+    }
+
+    @Override
+    public void onAttach(Context context)
+    {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener)
+        {
+            mListener = (OnFragmentInteractionListener) context;
+        } else
+        {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach()
+    {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        new RetrieveFeedTask().execute();
+    }
+
+    public interface OnFragmentInteractionListener
+    {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+
+
+    private void drawGraph()
+    {
+        LineData data = new LineData(xAxis, dataSets);
+        Log.i("DATA SETS", dataSets.toString());
+        chart.setData(data);
+        chart.setDescription("");
+        chart.animateY(2000);
+        chart.invalidate();
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setSpaceBetweenLabels(0);
+    }
+
+    class RetrieveFeedTask extends AsyncTask<Void, Void, String>
+    {
+
+        private Exception exception;
+
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+
+        }
+
+        protected String doInBackground(Void... urls) {
+            String email = "andrei.atanasiu1994@gmail.com";
+
+
+            try {
+                URL url = new URL(API_URL);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                String credentials = email + ":" + API_KEY;
+                String auth = "Basic" + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+                urlConnection.setRequestProperty("Authorization",auth);
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setRequestProperty("Accept", "application/json");
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response)
+        {
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            progressBar.setVisibility(View.GONE);
+            Log.i("INFO", response);
+
+            try {
+
+
+                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+                JSONArray items = object.getJSONArray("items");
+                ArrayList<Entry> valueSet1 = new ArrayList<>();
+                xAxis = new ArrayList<>();
+                int numberSearch = 0;
+                for(int i = 0; i < items.length(); i++)
+                {
+
+                    //Integer pages = items.getJSONObject(i).getInt("pages");
+                    Integer visits = items.getJSONObject(i).getInt("visits");
+                    String search_engine = items.getJSONObject(i).getString("search_engine");
+
+                    Entry entry = new Entry((float)visits, numberSearch);
+                    valueSet1.add(entry);
+                    xAxis.add(search_engine);
+                    numberSearch = numberSearch + 1;
+
+
+                }
+
+                LineDataSet lineDataSet1 = new LineDataSet(valueSet1, "VISITS");
+                lineDataSet1.setColor(Color.rgb(0, 0, 0));
+                dataSets = new ArrayList<>();
+                dataSets.add(lineDataSet1);
+
+                drawGraph();
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+
+}
