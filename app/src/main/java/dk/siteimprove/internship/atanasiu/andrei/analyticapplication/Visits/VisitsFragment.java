5b5bb0b00425108d16909c79f9928da5ea449d5e
Emil Madsen <Ostebob1234@gmail.com>
2016-07-29 15:40:48 +0200

    added some icons

--- app/src/main/java/dk/siteimprove/internship/atanasiu/andrei/analyticapplication/Visits/VisitsFragment.java
+++ app/src/main/java/dk/siteimprove/internship/atanasiu/andrei/analyticapplication/Visits/VisitsFragment.java
@@ -104,7 +104,10 @@ public class VisitsFragment extends Fragment implements View.OnClickListener
 
         textViewDate.setText("0 - 0");
         textViewInfo.setText("VISITS TODAY");
-        tableToggler.setText("Visits today");
+        tableToggler.setText("Visits today ");
+        tableToggler.setGravity(Gravity.LEFT);
+        tableToggler.setCompoundDrawablesWithIntrinsicBounds(null, null,
+                getResources().getDrawable(R.drawable.ic_keyboard_arrow_down_white_36dp), null);
         columnOne.setText("Hour of Day");
 
         tableToggler.setOnClickListener(this);
@@ -146,10 +149,14 @@ public class VisitsFragment extends Fragment implements View.OnClickListener
         {
             table.setVisibility(View.GONE);
             tableIsVisible = false;
+            tableToggler.setCompoundDrawablesWithIntrinsicBounds(null, null,
+                    getResources().getDrawable(R.drawable.ic_keyboard_arrow_down_white_36dp), null);
         }else
         {
             table.setVisibility(View.VISIBLE);
             tableIsVisible = true;
+            tableToggler.setCompoundDrawablesWithIntrinsicBounds(null, null,
+                    getResources().getDrawable(R.drawable.ic_keyboard_arrow_up_white_36dp), null);
         }
     }
 
@@ -158,7 +165,7 @@ public class VisitsFragment extends Fragment implements View.OnClickListener
 
         for (int i = 0; i < totalHours ; i++)
         {
-            TableRow[] tableRow = new TableRow[7];
+            TableRow[] tableRow = new TableRow[totalHours];
             tableRow[i] = new TableRow(getActivity());
             tableRow[i].setPadding(40, 40, 40, 40);
 
