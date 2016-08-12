fe2b89ec640915abf045fbec663e95243d5b5cb7
Emil Madsen <Ostebob1234@gmail.com>
2016-08-12 13:28:15 +0200

    fragment background is now gradient.

--- app/src/main/java/dk/siteimprove/internship/atanasiu/andrei/analyticapplication/Social_Media/SocialMediaFragment.java
+++ app/src/main/java/dk/siteimprove/internship/atanasiu/andrei/analyticapplication/Social_Media/SocialMediaFragment.java
@@ -248,7 +248,6 @@ public class SocialMediaFragment extends Fragment implements View.OnClickListene
         chart.setDescription("");
         chart.animateXY(1000, 1000);
         chart.invalidate();
-        chart.setBackgroundColor(Color.rgb(68, 68, 68));
         chart.setGridBackgroundColor(R.color.White);
         chart.getLegend().setTextColor(Color.WHITE);
         chart.getAxisLeft().setDrawLabels(false);
