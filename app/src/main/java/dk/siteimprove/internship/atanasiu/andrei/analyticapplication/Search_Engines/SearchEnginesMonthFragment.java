47afce29e1a5f75da1b6e5cf973ab936b7fa5331
Atanasiu <Andreww0007@gmail.com>
2016-08-29 13:47:19 +0200

    changed the method has network connection in search engine also simplified the landscapemode boolean

--- app/src/main/java/dk/siteimprove/internship/atanasiu/andrei/analyticapplication/Search_Engines/SearchEnginesMonthFragment.java
+++ app/src/main/java/dk/siteimprove/internship/atanasiu/andrei/analyticapplication/Search_Engines/SearchEnginesMonthFragment.java
@@ -84,14 +84,7 @@ public class SearchEnginesMonthFragment extends Fragment implements View.OnClick
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
-        {
-            landscapeMode = true;
-        }
-        else
-        {
-            landscapeMode = false;
-        }
+        landscapeMode = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
     }
 
     @Override
@@ -158,7 +151,7 @@ public class SearchEnginesMonthFragment extends Fragment implements View.OnClick
             apiIdSelected = false;
         }
 
-        if(haveNetworkConnection())
+        if(hasNetworkConnection())
         {
             if(apiIdSelected)
             {
@@ -255,22 +248,25 @@ public class SearchEnginesMonthFragment extends Fragment implements View.OnClick
         return period;
     }
 
-    public boolean haveNetworkConnection()
+    public boolean hasNetworkConnection()
     {
-        boolean haveConnectedWifi = false;
-        boolean haveConnectedMobile = false;
+        boolean isConnectedWifi = false;
+        boolean isConnectedMobile = false;
 
         ConnectivityManager cm = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
-        for (NetworkInfo ni : netInfo) {
-            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
-                if (ni.isConnected())
-                    haveConnectedWifi = true;
-            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
-                if (ni.isConnected())
-                    haveConnectedMobile = true;
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null) // connected to the internet
+        {
+            if (netInfo.getType() == ConnectivityManager.TYPE_WIFI)
+            {
+                isConnectedWifi = true;
+            }
+            if (netInfo.getType() == ConnectivityManager.TYPE_MOBILE)
+            {
+                isConnectedMobile = true;
+            }
         }
-        return haveConnectedWifi || haveConnectedMobile;
+        return isConnectedWifi || isConnectedMobile;
     }
 
     @Override
--- app/src/main/java/dk/siteimprove/internship/atanasiu/andrei/analyticapplication/Search_Engines/SearchEnginesWeekFragment.java
+++ app/src/main/java/dk/siteimprove/internship/atanasiu/andrei/analyticapplication/Search_Engines/SearchEnginesWeekFragment.java
@@ -84,14 +84,7 @@ public class SearchEnginesWeekFragment extends Fragment implements View.OnClickL
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
-        {
-            landscapeMode = true;
-        }
-        else
-        {
-            landscapeMode = false;
-        }
+        landscapeMode = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
     }
 
     @Override
@@ -151,7 +144,7 @@ public class SearchEnginesWeekFragment extends Fragment implements View.OnClickL
             apiIdSelected = false;
         }
 
-        if(haveNetworkConnection())
+        if(hasNetworkConnection())
         {
             if(apiIdSelected)
             {
@@ -244,22 +237,25 @@ public class SearchEnginesWeekFragment extends Fragment implements View.OnClickL
         new RetrieveFeedTask().execute();
     }
 
-    public boolean haveNetworkConnection()
+    public boolean hasNetworkConnection()
     {
-        boolean haveConnectedWifi = false;
-        boolean haveConnectedMobile = false;
+        boolean isConnectedWifi = false;
+        boolean isConnectedMobile = false;
 
         ConnectivityManager cm = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
-        for (NetworkInfo ni : netInfo) {
-            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
-                if (ni.isConnected())
-                    haveConnectedWifi = true;
-            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
-                if (ni.isConnected())
-                    haveConnectedMobile = true;
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null) // connected to the internet
+        {
+            if (netInfo.getType() == ConnectivityManager.TYPE_WIFI)
+            {
+                isConnectedWifi = true;
+            }
+            if (netInfo.getType() == ConnectivityManager.TYPE_MOBILE)
+            {
+                isConnectedMobile = true;
+            }
         }
-        return haveConnectedWifi || haveConnectedMobile;
+        return isConnectedWifi || isConnectedMobile;
     }
 
     @Override
